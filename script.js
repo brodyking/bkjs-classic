87d3e354ece6a4a1ea6ec3eb201b44ad4828338f
brodyking <bk.2k@hotmail.com>
2022-04-01 21:15:08 -0500

    Added the code for a footer.

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.getElementById('nav').innerHTML = '<a href="../pages/" rel="nofollow">p
 } else {
 document.getElementById('nav2').innerHTML = '<a href="../../pages/" rel="nofollow">pages</a> &middot; <a href="../../projects/" rel="nofollow">projects</a> &middot; <a href="../../blog/" rel="nofollow">blog</a>';
 }
+
+
+// var footerbody = "&copy; 2022 All Rights Reserved."
+// document.body.innerHTML += "<center style='margin-top: 15%;'>" + footerbody + "</center>";
